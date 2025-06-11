790b08396949b4b77a07e6245fc3500124702398
Jose Vinet <jvinetconsola@gmail.com>
2025-06-11 10:38:46 -0400

    configuracion netlify

--- app.js
+++ app.js
@@ -12,15 +12,20 @@ import bodyParser from "body-parser";
 config();
 
 const app = express();
+let prefixNetlify='';
 
 app.use(express.json());
 app.use(cors());
-app.use("/services", servicesRoutes);
-app.use("/users", usersRoutes);
-app.use("/images", imagesRoutes);
-app.use("/comments", commentRoutes);
-app.use("/favorites", favoriteRoutes);
-app.use("/uploads", express.static("uploads"));
+if(process.env.NETLIFY){
+    prefixNetlify='/.netlify/functions';
+}
+
+app.use(`${prefixNetlify}/services`, servicesRoutes);
+app.use(`${prefixNetlify}/users`, usersRoutes);
+app.use(`${prefixNetlify}/images`, imagesRoutes);
+app.use(`${prefixNetlify}/comments`, commentRoutes);
+app.use(`${prefixNetlify}/favorites`, favoriteRoutes);
+app.use(`${prefixNetlify}/uploads`, express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 export default app;
