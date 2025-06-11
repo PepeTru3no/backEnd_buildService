e7fdc4edff8c4ca5536266dff716bd544e065a17
Jose Vinet <jvinetconsola@gmail.com>
2025-06-11 11:25:10 -0400

    correccion error en pg

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import { config } from 'dotenv';
+import pg from 'pg';
 config();
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -7,6 +8,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    dialectModule: pg, //especifica pg a utilizar
     dialectOptions: {
         ssl: {
             require: true, // Enforce SSL
